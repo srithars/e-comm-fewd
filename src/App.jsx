43e1f1e1c5bebacc1d0f73acd83b28f64d32033c
srithars <64460687+srithars@users.noreply.github.com>
2026-02-22 19:48:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Products from "./components/Products";
-import "./components/styles.css"
+import Products1 from "./components/Products1";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
-import Products1 from "./components/Products1";
 import Signup from "./components/Signup";
-
-
+import "./components/styles.css";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>  
-        {/* <Route path="products" element={<Products />} />   */}
-        <Route path="products" element={<Products1/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="cart" element={<Cart/>}/>
-        <Route path="signup" element={<Signup/>}/>
-         
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products1 />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </HashRouter>
   );
